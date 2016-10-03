ee3f433eeeb0c2c492f58a373df5cb3b07ba4249
Prsenjit Goswami <Prosenjit_Goswami@BBCiMAC.local>
2016-10-03 19:44:17 +0600

    Commit2

--- TestProject0001/AppDelegate.m
+++ TestProject0001/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
-        //2
+      
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
